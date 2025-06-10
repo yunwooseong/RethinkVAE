030f0b36cc63b34c57af6801433b07b2ff741f2e
Woo-Seong Yun <101303498+yunwooseong@users.noreply.github.com>
2025-06-10 10:46:59 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,6 @@
-CUDA_VISIBLE_DEVICES=4 python main.py -d=ML1M -ne=500 -k=20 -ls=0.0
-CUDA_VISIBLE_DEVICES=4 python main.py -d=ML1M -ne=500 -k=20 -ls=0.05
+CUDA_VISIBLE_DEVICES=0 python main.py -d=ML1M -ne=500 -k=20 -ls=0.0
+CUDA_VISIBLE_DEVICES=0 python main.py -d=ML1M -ne=500 -k=20 -ls=0.05
+CUDA_VISIBLE_DEVICES=0 python main.py -d=AKindle -ne=500 -k=20 -ls=0.0
+CUDA_VISIBLE_DEVICES=0 python main.py -d=AKindle -ne=500 -k=20 -ls=0.001
+CUDA_VISIBLE_DEVICES=0 python main.py -d=Yelp -ne=200 -k=20 -ls=0.0
+CUDA_VISIBLE_DEVICES=0 python main.py -d=Yelp -ne=200 -k=20 -ls=0.0005
